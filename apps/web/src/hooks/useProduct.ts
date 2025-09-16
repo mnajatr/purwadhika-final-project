a0a60eb40716d3be0807b17574d34bebb0e4b4b2
ImeliaDwinora <imeliadwinora@gmail.com>
2025-09-16 19:22:06 +0700

    feat(web): add update and delete product features
    - Implement product create and update form using React Hook Form
    - Add integration with API for updating product by slug

--- apps/web/src/hooks/useProduct.ts
+++ apps/web/src/hooks/useProduct.ts
@@ -2,7 +2,10 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import {
   getProductBySlug,
   productsService,
+  deleteProduct,
+  updateProduct,
 } from "../services/products.service";
+import { ProductResponse } from "@/types/products.type";
 
 export function useProducts() {
   return useQuery({
@@ -24,6 +27,36 @@ export function useCreateProduct() {
   return useMutation({
     mutationFn: (data: any) =>
       productsService.createProduct(data).then((res) => res),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+  });
+}
+
+export function useUpdateProduct() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      slug,
+      data,
+    }: {
+      slug: string;
+      data: Partial<ProductResponse>;
+    }) => updateProduct(slug, data),
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      queryClient.invalidateQueries({ queryKey: ["product", variables.slug] });
+    },
+  });
+}
+
+// 🔹 Delete product
+export function useDeleteProduct() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (slug: string) => deleteProduct(slug),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["products"] });
     },
